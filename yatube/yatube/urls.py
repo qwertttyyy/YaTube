256cfc0f7e9cef959c25f31d67e9a5b2b35ab8ad
qwertttyyy <zelmisha2@gmail.com>
2023-02-12 02:43:28 +0300

    bug fix

--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 handler404 = 'core.views.page_not_found'
+handler403 = 'core.views.csrf_failure'
 
 urlpatterns = [
     path('', include('posts.urls', namespace='posts')),
